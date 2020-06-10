d852dd0625273d18ecd80d1debac6cfc0b2f2b60
Sandeep <dhalosandeepthakur@gmail.com>
2020-06-11 01:22:23 +0530

    First Commit

---
+++ src/test/java/com/sandeep/ws/Micro2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
